6b7ce4e5431ea84109ea410d48e8f0fa907ec9b9
branqa <branqas@gmail.com>
2018-03-05 21:48:28 +0100

    rename

--- exam_statistics/JS/student.js
+++
@@ -1,8 +0,0 @@
-function Student (name, surname){
-    this.name = name;
-    this.surname = surname;
-}
-
-Student.prototype.getStudentData = function(){
-    return this.name + ' ' + this.surname;
-}
--- exam_statistics/JS/subject.js
+++
@@ -1,7 +0,0 @@
-function  Subject(nameOfSubject) {
-    this.nameOfSubject = nameOfSubject;
-}
-
-Subject.prototype.getSubjectName = function (){
-    return this.nameOfSubject;
-}
